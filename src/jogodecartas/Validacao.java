bf3bda31e30bb3130a3db4400e1727031bf8da48
Eric <ericosta.dev@gmail.com>
2019-09-17 20:52:24 -0300

    iniciar repositório

---
+++ src/jogodecartas/Validacao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jogodecartas;
+
+/**
+ *
+ * @author eric
+ */
+public class Validacao {
+    
+}
+/*
+Ganha se tiver ij    e   ij[1] e   ij[2]
+Ganha se tiver i j   e   i+1 j e   i+1+1 j
+*/
